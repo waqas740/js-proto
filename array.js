8f8db7c39f9a552dd15361b8dec7a1d34099264c
waqas-740 <waqas.iqbal@frizhub.com>
2022-07-09 05:13:42 +0500

    update array method

--- array.js
+++ array.js
@@ -1,73 +1,77 @@
 Array.prototype.toFLUpperCase = function () {
-    return this.map(str => str.charAt(0).toUpperCase() + str.slice(1))
-}
+  return this.map((str) =>
+    typeof str == "string" ? str.toFLUpperCase() : str
+  );
+};
 Array.prototype.toFLLowerCase = function () {
-    return this.map(str => str.charAt(0).toLowerCase() + str.slice(1))
-}
+  return this.map((str) =>
+    typeof str == "string" ? str.toFLLowerCase() : str
+  );
+};
 Array.prototype.toLowerCase = function () {
-    return this.map(str => str.trim().toLowerCase())
-}
+  return this.map((str) => (typeof str == "string" ? str.toLowerCase() : str));
+};
 Array.prototype.toUpperCase = function () {
-    return this.map(str => str.trim().toUpperCase())
-}
+  return this.map((str) => (typeof str == "string" ? str.toUpperCase() : str));
+};
 Array.prototype.sum = function () {
-    return this.reduce((total, num) => {
-        total += num
-    }, 0)
-}
+  return this.reduce((total, num) => {
+    if (typeof num === "number") {
+      total += num;
+    }
+    return total;
+  }, 0);
+};
 Array.prototype.min = function () {
-    return this.reduce((min, num) => {
-        if (min > num) min = num;
-    }, 0)
-}
+  return this.reduce((min, num) => {
+    if (typeof num === "number" && min > num) min = num;
+    return min;
+  }, this[0]);
+};
 Array.prototype.max = function () {
-    return this.reduce((max, num) => {
-        if (max < num) max = num;
-    }, 0)
-}
+  return this.reduce((max, num) => {
+    if (typeof num == "number" && max < num) max = num;
+    return max;
+  }, this[0]);
+};
 Array.prototype.random = function () {
-    return this[Math.floor(Math.random() * this.length)]
-}
+  return this[Math.floor(Math.random() * this.length)];
+};
 Array.prototype.chunk = function (size) {
-    let splitSize = this.length / size;
-    let newArr = [];
-    let startIndex = 0;
-    let endIndex = size;
-    for (let i = 0; i < splitSize; i++) {
-        let chunk = this.slice(startIndex, endIndex);
-        newArr.push(chunk)
-        startIndex = startIndex + size;
-        endIndex = endIndex + size;
+  var splitSize = this.length / size;
+  var newArr = [];
+  var startIndex = 0;
+  var endIndex = size;
+  for (var i = 0; i < splitSize; i++) {
+    var chunk = this.slice(startIndex, endIndex);
+    newArr.push(chunk);
+    startIndex = startIndex + size;
+    endIndex = endIndex + size;
+  }
+  return newArr;
+};
+Array.prototype.isEquivalent = function (second) {
+  var firstProps = Object.getOwnPropertyNames(this);
+  var secondProps = Object.getOwnPropertyNames(second);
+
+  if (firstProps.length != secondProps.length) {
+    return false;
+  }
+
+  for (var i = 0; i < firstProps.length; i++) {
+    var propName = firstProps[i];
+    if (this[propName] !== second[propName]) {
+      return false;
     }
-    return newArr;
-}
-Array.prototype.isEquivalent = function (first, second) {
-    // Create arrays of property names
-    var firstProps = Object.getOwnPropertyNames(first);
-    var secondProps = Object.getOwnPropertyNames(second);
+  }
 
-    // If number of properties is different,
-    // objects are not equivalent
-    if (firstProps.length != secondProps.length) {
-        return false;
-    }
-
-    for (var i = 0; i < firstProps.length; i++) {
-        var propName = firstProps[i];
-
-        // If values of same property are not equal,
-        // objects are not equivalent
-        if (first[propName] !== second[propName]) {
-            return false;
-        }
-    }
-
-    // If we made it this far, objects
-    // are considered equivalent
-    return true;
-}
+  // If we made it this far, objects
+  // are considered equivalent
+  return true;
+};
 Array.prototype.unique = function () {
-    const newSet = new Set([...this]);
-    return [...newSet];
-}
-
+  var arr = this.map((item) => JSON.stringify(item));
+  return this.filter(
+    (item, index) => arr.indexOf(JSON.stringify(item)) === index
+  );
+};
