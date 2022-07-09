a1ff935b1a83c0f3ade7070fda97373780ffe027
waqas-740 <waqas.iqbal@frizhub.com>
2022-07-10 03:25:04 +0500

    update object method

--- object.js
+++ object.js
@@ -1,70 +1,85 @@
-Object.prototype.flip = function(){
+!Object.prototype.flip &&
+  (Object.prototype.flip = function () {
     for (const [key, value] of Object.entries(this)) {
-       this[value] = key
-        delete this[key]
+      this[value] = key;
+      delete this[key];
     }
-    
-}
-Object.prototype.sort = function(){
-    return Object.keys(this).sort().reduce(
-        (obj, key) => { 
-          obj[key] = this[key]; 
-          return obj;
-        }, 
-        {}
-      );
-}
-Object.prototype.pick = function(path){
-    const result = {} 
-    for(let key of path){
-        this[key]&& (result[key] = this[key])
+  });
+
+!Object.prototype.sort &&
+  (Object.prototype.sort = function () {
+    return Object.keys(this)
+      .sort()
+      .reduce((obj, key) => {
+        obj[key] = this[key];
+        return obj;
+      }, {});
+  });
+!Object.prototype.pick &&
+  (Object.prototype.pick = function (path) {
+    const result = {};
+    for (let key of path) {
+      this[key] && (result[key] = this[key]);
     }
-    return result
-}
-Object.prototype.omit = function(path){
-    const result = Object.assign({},this)
-    for(let key of path){
-        delete result[key]
+    return result;
+  });
+!Object.prototype.omit &&
+  (Object.prototype.omit = function (path) {
+    const result = Object.assign({}, this);
+    for (let key of path) {
+      delete result[key];
     }
-    return result
-}
-Object.prototype.clone = function(){
-    return Object.assign({},this)
-}
-Object.prototype.merge = function(...objects){
-    return Object.assign(this,...objects)
-    
-}
-Object.prototype.size = function(){
+    return result;
+  });
+!Object.prototype.clone &&
+  (Object.prototype.clone = function () {
+    return Object.assign({}, this);
+  });
+!Object.prototype.merge &&
+  (Object.prototype.merge = function (objects) {
+    var _this = Object.assign({}, this);
+    arguments.forEach(function (obj) {
+      Object.assign(_this, obj);
+    });
+    return _this;
+  });
+!Object.prototype.size &&
+  (Object.prototype.size = function () {
     return Object.keys(this).length;
-}
-Object.prototype.keys = function(){
+  });
+!Object.prototype.keys &&
+  (Object.prototype.keys = function () {
     return Object.keys(this);
-}
-Object.prototype.values = function(){
+  });
+!Object.prototype.values &&
+  (Object.prototype.values = function () {
     return Object.values(this);
-}
-Object.prototype.FLUpperCase = function(){
-    this.keys().forEach(key=>{
-        this[key.toFLUpperCase()]
-        delete this[key]
-    })
-}
-Object.prototype.FLLowerCase = function(){
-    this.keys().forEach(key=>{
-        this[key.toFLLowerCase()]
-        delete this[key]
-    })
-}
-Object.prototype.lowerCase = function(){
-    this.keys().forEach(key=>{
-        this[key.toLowerCase()]
-        delete this[key]
-    })
-}
-Object.prototype.upperCase = function(){
-    this.keys().forEach(key=>{
-        this[key.toUpperCase()]
-        delete this[key]
-    })
-}
+  });
+!Object.prototype.FLUpperCase &&
+  (Object.prototype.FLUpperCase = function () {
+    Object.key(this).forEach((key) => {
+      this[key.toFLUpperCase()];
+      delete this[key];
+    });
+  });
+!Object.prototype.FLLowerCase &&
+  (Object.prototype.FLLowerCase = function () {
+    Object.key(this).forEach((key) => {
+      this[key.toFLLowerCase()];
+      delete this[key];
+    });
+  });
+!Object.prototype.lowerCase &&
+  (Object.prototype.lowerCase = function () {
+    Object.key(this).forEach((key) => {
+      this[key.toLowerCase()];
+      delete this[key];
+    });
+  });
+!Object.prototype.upperCase &&
+  (Object.prototype.upperCase = function () {
+    Object.key(this).forEach((key) => {
+      this[key.toUpperCase()];
+      delete this[key];
+    });
+  });
