56f544182ccfefb75688547019d48e54f99b2426
waqas-740 <waqas.iqbal@frizhub.com>
2022-07-09 05:15:22 +0500

    setup array test case with jest

---
+++ tests/array.test.js
@@ -0,0 +1,315 @@
+require("../index.js");
+
+describe("Array", function () {
+  describe("#toFLUpperCase()", function () {
+    it(
+      "should return same array with first letter of  strings in uppercase",
+      function () {
+        expect(
+          [
+            1,
+            2,
+            3,
+            "4",
+            [1, "3"],
+            { a: 1 },
+            "a",
+            "hello",
+            "WORD",
+            "lETTER",
+          ].toFLUpperCase()
+        ).toStrictEqual([
+          1,
+          2,
+          3,
+          "4",
+          [1, "3"],
+          { a: 1 },
+          "A",
+          "Hello",
+          "WORD",
+          "LETTER",
+        ]);
+      }.bind(this)
+    );
+  });
+
+  describe("#toFLLowerCase()", function () {
+    it(
+      "should return same array with first letter of  strings in lowercase",
+      function () {
+        expect(
+          [
+            1,
+            2,
+            3,
+            "4",
+            [1, "3"],
+            { a: 1 },
+            "A",
+            "Hello",
+            "WORD",
+            "LETTER",
+          ].toFLLowerCase()
+        ).toStrictEqual([
+          1,
+          2,
+          3,
+          "4",
+          [1, "3"],
+          { a: 1 },
+          "a",
+          "hello",
+          "wORD",
+          "lETTER",
+        ]);
+      }.bind(this)
+    );
+  });
+
+  describe("#toLowerCase()", function () {
+    it(
+      "should return same array with all  strings in lowercase",
+      function () {
+        expect(
+          [
+            1,
+            2,
+            3,
+            "4",
+            [1, "3"],
+            { a: 1 },
+            "A",
+            "hello",
+            "WORD",
+            "LeTtER",
+          ].toLowerCase()
+        ).toStrictEqual([
+          1,
+          2,
+          3,
+          "4",
+          [1, "3"],
+          { a: 1 },
+          "a",
+          "hello",
+          "word",
+          "letter",
+        ]);
+      }.bind(this)
+    );
+  });
+
+  describe("#toUpperCase()", function () {
+    it(
+      "should return same array with all  strings in uppercase",
+      function () {
+        expect(
+          [
+            1,
+            2,
+            3,
+            "4",
+            [1, "3"],
+            { a: 1 },
+            "a",
+            "Hello",
+            "WORD",
+            "LeTTer",
+          ].toUpperCase()
+        ).toStrictEqual([
+          1,
+          2,
+          3,
+          "4",
+          [1, "3"],
+          { a: 1 },
+          "A",
+          "HELLO",
+          "WORD",
+          "LETTER",
+        ]);
+      }.bind(this)
+    );
+  });
+
+  describe("#sum()", function () {
+    it(
+      "should return sum of all numbers in array level 1 or 0 if array is empty",
+      function () {
+        expect(
+          [
+            1,
+            2,
+            3,
+            "4",
+            [1, "3"],
+            { a: 1 },
+            null,
+            undefined,
+            "a",
+            "Hello",
+            "WORD",
+            "LeTTer",
+          ].sum()
+        ).toBe(6);
+      }.bind(this)
+    );
+  });
+  describe("#min()", function () {
+    it(
+      "should return min of all numbers in array level 1 or 0 if array is empty",
+      function () {
+        expect(
+          [
+            1,
+            2,
+            3,
+            "4",
+            [1, "3"],
+            [-1],
+            { a: 1 },
+            null,
+            undefined,
+            "a",
+            "Hello",
+            "WORD",
+            "LeTTer",
+          ].min()
+        ).toBe(1);
+      }.bind(this)
+    );
+  });
+  describe("#max()", function () {
+    it(
+      "should return max of all numbers in array level 1 or 0 if array is empty",
+      function () {
+        expect(
+          [
+            1,
+            2,
+            3,
+            "4",
+            [1, "3"],
+            { a: 1 },
+            null,
+            undefined,
+            [8],
+            "a",
+            "Hello",
+            "WORD",
+            "LeTTer",
+          ].max()
+        ).toBe(3);
+      }.bind(this)
+    );
+  });
+  describe("#chunk()", function () {
+    it(
+      "should return chunked array",
+      function () {
+        expect(
+          [
+            1,
+            2,
+            3,
+            "4",
+            [1, "3"],
+            { a: 1 },
+            null,
+            undefined,
+            [8],
+            "a",
+            "Hello",
+            "WORD",
+            "LeTTer",
+          ].chunk(5)
+        ).toStrictEqual([
+          [1, 2, 3, "4", [1, "3"]],
+          [{ a: 1 }, null, undefined, [8], "a"],
+          ["Hello", "WORD", "LeTTer"],
+        ]);
+      }.bind(this)
+    );
+  });
+  describe("#isEquivalent()", function () {
+    it(
+      "should return true if arrays are equivalent",
+      function () {
+        expect(
+          [
+            1,
+            2,
+            3,
+            "4",
+            [1, "3"],
+            { a: 1 },
+            null,
+            undefined,
+            [8],
+            "a",
+            "Hello",
+            "WORD",
+            "LeTTer",
+          ].isEquivalent([
+            1,
+            2,
+            3,
+            "4",
+            [1, "3"],
+            { a: 1 },
+            null,
+            undefined,
+            [8],
+            "a",
+            "Hello",
+            "WORD",
+            "LeTTer",
+          ])
+        ).toBe(true);
+      }.bind(this)
+    );
+  });
+  describe("#unique()", function () {
+    it(
+      "should return unique array",
+      function () {
+        expect(
+          [
+            1,
+            2,
+            3,
+            "4",
+            [1, "3"],
+            2,
+            "4",
+            { a: 1 },
+            { a: 1 },
+            null,
+            undefined,
+            [8],
+            "a",
+            "A",
+            "a",
+            "LeTTer",
+            [8],
+            "LeTTer",
+          ].unique()
+        ).toStrictEqual([
+          1,
+          2,
+          3,
+          "4",
+          [1, "3"],
+          { a: 1 },
+          null,
+          undefined,
+          [8],
+          "a",
+          "A",
+          "LeTTer",
+        ]);
+      }.bind(this)
+    );
+  });
+});
