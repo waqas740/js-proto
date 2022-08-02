770e85f06cd6f056298f056b3238544d0fa1580c
waqas-740 <waqas.iqbal@frizhub.com>
2022-08-02 18:45:09 +0500

    add init function in index

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-require('./array')
-require('./object.js')
-require('./string')
+let addArrayProto = true;
+let addObjectProto = true;
+let addStringProto = true;
+/**
+ *
+ * @param {Object} options
+ * @param {Boolean} options.Array
+ *  @param {Boolean} options.Object
+ * @param {Boolean} options.String
+ *
+ */
+module.exports.init = (options) => {
+  if (options) {
+    if (typeof options !== "object" || Array.isArray(options)) {
+      throw new Error(
+        "options should be object and have at least one key like Array or Object or String with boolean value"
+      );
+    }
+
+    (!options.hasOwnProperty("Array") || !options.Array == false) &&
+      (addArrayProto = false);
+    (!options.hasOwnProperty("Object") || !options.Object == false) &&
+      (addObjectProto = false);
+    (!options.hasOwnProperty("String") || !options.String == false) &&
+      (addStringProto = false);
+  }
+  addArrayProto && require("./array");
+  addObjectProto && require("./object");
+  addStringProto && require("./string");
+};
+module.exports.init();
